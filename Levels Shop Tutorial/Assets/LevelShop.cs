0d67cb3d7d8d4a82824086665611b38d3ea278cc
Ordaricc <moriniriccardo97@gmail.com>
2020-10-05 21:48:35 +0200

    Created project

---
+++ Levels Shop Tutorial/Assets/LevelShop.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelShop : MonoBehaviour
+{
+    public TextMeshProUGUI buttonText;
+
+    public bool isLevelUnlocked;
+    public int indexOfSceneToLoad;
+    public int levelCost;
+
+    private void Awake()
+    {
+        //check if level was already bought
+        if (PlayerPrefs.GetInt("prefLevel" + indexOfSceneToLoad) == 1)
+        {
+            isLevelUnlocked = true;
+            buttonText.text = "Enter Level";
+        }
+        else
+        {
+            buttonText.text = "Buy level for: " + levelCost;
+        }
+    }
+
+    public void OnLevelButtonPress()
+    {
+        if (isLevelUnlocked)//enter level
+        {
+            SceneManager.LoadScene(indexOfSceneToLoad);
+        }
+        else//try buying level
+        {
+            if (FindObjectOfType<PlayerMoney>().TryRemoveMoney(levelCost))
+            {
+                //level was bought
+                isLevelUnlocked = true;
+                PlayerPrefs.SetInt("prefLevel" + indexOfSceneToLoad, 1);
+                buttonText.text = "Enter Level";
+            }
+        }
+    }
+}
---
+++ Levels Shop Tutorial/Assets/PlayerMoney.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    public int money;
+
+    public bool TryRemoveMoney(int moneyToRemove)
+    {
+        if (money >= moneyToRemove)//do we have enough money
+        {
+            money -= moneyToRemove;//remove money
+            return true;//tell the transaction was successfull
+        }
+        else//don't have enough money
+        {
+            return false;//transaction NOT successfull
+        }
+    }
+}
